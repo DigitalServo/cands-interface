849803800d4d10b76d2a5ba5b6d1f2b2309a89d6
wcrvt <cat0127.k@gmail.com>
2024-09-26 18:34:52 +0900

    rexport setting constant of raspberrypi

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,7 @@ pub use tcan4550::register as tcan4550_register;
 
 pub use tranceiver::TCAN455xTranceiver;
 
+#[cfg(feature="raspberrypi")]
+pub use device_driver::raspberrypi::GPIO_INPUT_PIN_NUM;
+
 pub use rx_buffer::RxData;
